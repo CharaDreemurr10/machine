33db80bba8ac07770851db235d359bf304933d98
Ni Rimmy <uwaifu2@gmail.com>
2019-04-20 12:37:42 +0600

    initial commit

---
+++ oof.js
@@ -0,0 +1,19 @@
+//alert("Javacript подключен");
+
+workon = false;
+q = "A";
+step = 0;
+cells = new Array(15);
+select = 7;
+
+function startui(){
+    workon = true;
+}
+
+function stopp(){
+    workon = false;
+}
+
+function go_step(){
+    
+}
